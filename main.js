84f26c9ae9b6683690b7c18e321e742df7b20120
Kadeisha93 <69065736+Kadeisha93@users.noreply.github.com>
2020-08-19 22:12:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+function date(){
+  var month=document.getElementById('month').value.toLowerCase()
+  var day= document.getElementById('day').value
+  var signs=document.getElementById('signs')
+
+  day=parseInt(day)
+  horoscopes(month,day,signs)
+}
+
+function horoscopes(month,day,signs){
+  if ((month==='january' && day>=20)||(month==='february' && day<=18)) {
+    signs.innerHTML='Aquarius'
+  }
+  else if ((month==='february' && day>=19) || (month==='march' && day<=20)) {
+    signs.innerHTML='Pisces'
+  }
+  else if((month==='march' && day>=21) || (month==='april' && day<=19)) {
+    signs.innerHTML='Aries'
+  }
+  else if ((month==='april' && day>=20) || (month==='may' && day<=20)) {
+    signs.innerHTML='Taurus'
+  }
+  else if ((month==='may' && day>=21) || (month==='june' && day<=20)) {
+    signs.innerHTML='Gemini'
+  }
+  else if ((month==='june' && day>=21) || (month==='july' && day<=22)) {
+    signs.innerHTML='Cancer'
+  }
+  else if((month==='july' && day>=23) || (month==='august' && day<=22)) {
+    signs.innerHTML='Leo'
+  }
+  else if ((month==='august' && day>=23) || (month==='september' && day<=22)) {
+    signs.innerHTML='Virgo'
+  }
+  else if ((month==='september' && day>=23) || (month==='october' && day<=22)) {
+    signs.innerHTML='Libra'
+  }
+  else if ((month==='october' && day>=23) || (month==='november' && day<=21)) {
+    signs.innerHTML='Scorpio'
+  }
+  else if ((month==='november' && day>=22) || (month==='december' && day<=21)) {
+    signs.innerHTML='Sagittarius'
+  }
+  else if ((month==='december' && day>=22) || (month==='january' && day<=19)) {
+    signs.innerHTML='Capricorn'
+  }
+  else if (day>=32){
+    signs.innerHTML='Try Again'
+  }
+}
+  document.getElementById('resButt').onclick=date
